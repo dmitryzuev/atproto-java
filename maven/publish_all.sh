dd20633d6b799c3a6621e326825a1de4f03d626d
Dmitry Zuev <mail@dmitryzuev.com>
2026-05-13 23:08:02 +0100

    fix maven publish

--- maven/publish_all.sh
+++ maven/publish_all.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 MODULES=(
-  "//src/java/com/atproto/lex/data:lex_data.publish"
-  "//src/java/com/atproto/lex/json:lex_json.publish"
-  "//src/java/com/atproto/lex/cbor:lex_cbor.publish"
-  "//src/java/com/atproto/lexicon:lexicon.publish"
-  "//src/java/com/atproto/xrpc:xrpc.publish"
+  "//src/java/com/atproto/lex/data:lex_data_export"
+  "//src/java/com/atproto/lex/json:lex_json_export"
+  "//src/java/com/atproto/lex/cbor:lex_cbor_export"
+  "//src/java/com/atproto/lexicon:lexicon_export"
+  "//src/java/com/atproto/xrpc:xrpc_export"
 )
 
 for target in "${MODULES[@]}"; do
